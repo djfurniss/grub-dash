9ef5b011bf28475a3e5444a551374ef84bc672f6
Denasia <dfurniss@ymail.com>
2022-09-02 12:08:24 -0400

    added property validations for dishes route

--- src/dishes/dishes.controller.js
+++ src/dishes/dishes.controller.js
@@ -5,6 +5,34 @@ const dishes = require(path.resolve("src/data/dishes-data"));
 
 // this function assigns ID's when necessary
 const nextId = require("../utils/nextId");
+ 
+// --- VALIDATION ---
+const dishExists = (req, res, next) => {
+    const { dishId } = req.params
+    const foundDish = dishes.find(dish => dish.id === dishId);
+    if (foundDish) {
+        res.locals.dish = foundDish
+        next();
+    }else{
+        next({status: 404, message: `dish ID: ${dishId} does not exist`});
+    };
+};
+
+const hasProperty = (property) => { //for create and update
+    return function (req, res, next){
+        const {data = {}} = req.body
+        if (!data[property]) { //runs for empty name, desc, and image_url strings
+            res.status(400).json({error:`Dish must include a ${property}`});
+        } else next();
+    };
+};
+
+const hasValidPrice = (req, res, next) => {
+    const {data: { price } = {}} = req.body
+    if (price <= 0 || !Number.isInteger(price)){
+        res.status(400).json({error: `Dish must have a price that is an integer greater than 0`})
+    } else next();
+};
 
 // --- HANDLERS ---
 function list (req, res, next) {
@@ -26,25 +54,33 @@ function create (req, res, next) {
 };
 
 function read (req, res, next) {
-    const { dishId } = req.params
-    const foundDish = dishes.find(dish => dish.id === dishId)
-    res.json({data: foundDish})
+    res.json({data: res.locals.dish})
 };
 
 function update (req, res, next) {
-    const { dishId } = req.params;
-    const foundDish = dishes.find(dish => dish.id === dishId)
+    foundDish = res.locals.dish
     const {data: {name, description, price, image_url} = {}} = req.body
-    foundDish.name = name
-    foundDish.description = description
-    foundDish.price = price
-    foundDish.image_url = image_url
+    foundDish = {name, description, price, image_url}
     res.json({data: foundDish})
 };
 
 module.exports = {
     list, 
-    create, 
-    read, 
-    update,
+    create: [
+        hasProperty("name"),
+        hasProperty("description"),
+        hasProperty("price"),
+        hasProperty("image_url"),
+        hasValidPrice,
+        create
+    ], 
+    read: [dishExists, read], 
+    update: [
+        dishExists, 
+        hasProperty("name"),
+        hasProperty("description"),
+        hasProperty("price"),
+        hasProperty("image_url"),
+        hasValidPrice,
+        update],
 };
