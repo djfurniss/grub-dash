6c011948571610d6e53d5046b654de867f706079
Denasia <dfurniss@ymail.com>
2022-09-15 18:16:21 -0400

    create dish migraton

--- src/db/migrations/20220915012438_createDishesTable.js
+++ src/db/migrations/20220915012438_createDishesTable.js
@@ -3,7 +3,13 @@
  * @returns { Promise<void> }
  */
 exports.up = function(knex) {
-  
+  return knex.schema.createTable("dishes", (table) => {
+    table.increments("dish_id").primary()
+    table.string("dish_name")
+    table.text("dish_description")
+    table.integer("dish_price")
+    table.string("dish_img_url")
+  })
 };
 
 /**
@@ -11,5 +17,5 @@ exports.up = function(knex) {
  * @returns { Promise<void> }
  */
 exports.down = function(knex) {
-  
+  return knex.schema.dropTable("dishes")
 };
